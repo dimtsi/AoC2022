09625e2cc3cb4aac987eb7c0957782f06f11a87a
Dimitris Tsimpoukis <d.tsimpoukis@kaizengaming.com>
2022-12-06 08:39:17 +0200

    day6

---
+++ Day6/day6.py
@@ -0,0 +1,84 @@
+from collections import Counter, defaultdict, deque
+from datetime import datetime
+from functools import reduce, lru_cache
+from typing import (
+    List,
+    Tuple,
+    Set,
+    Dict,
+    Iterable,
+    DefaultDict,
+    Optional,
+    Union,
+    Generator,
+)
+from copy import deepcopy
+from heapq import heappop, heappush
+import string
+
+import re
+
+
+def parse(filename: str):
+    with open(filename, "r") as f:
+        lines: List[str] = f.read().strip().split("\n")
+
+    return lines[0]
+
+
+def run(lines, p2=False):
+    valid = 0
+    buff = deque([])
+    for i, c in enumerate(lines):
+        buff.append(c)
+        if len(set(buff)) == (14 if p2 else 4):
+            break
+        if i > (12 if p2 else 2):
+            buff.popleft()
+    return i + 1
+
+
+def main(filename: str) -> Tuple[Optional[int], Optional[int]]:
+    from time import time
+
+    start = time()
+    answer_a, answer_b = None, None
+
+    lines = parse(filename)
+    answer_a = run(lines)
+    lines = parse(filename)
+    answer_b = run(lines, p2=True)
+
+    end = time()
+    print(end - start)
+    return answer_a, answer_b
+
+
+if __name__ == "__main__":
+
+    from utils import submit_answer
+    from aocd.exceptions import AocdError
+
+    sample = "sample.txt"
+    input = "input.txt"
+
+    sample_a_answer = 7
+    sample_b_answer = 19
+
+    answer_a, answer_b = main(sample)
+    assert (
+        answer_a == sample_a_answer
+    ), f"AnswerA incorrect: Actual: {answer_a}, Expected: {sample_a_answer}"
+    print("sampleA correct")
+    if answer_b:
+        assert (
+            answer_b == sample_b_answer
+        ), f"AnswerB incorrect: Actual: {answer_b}, Expected: {sample_b_answer}"
+        print("sampleB correct")
+
+    # Test on your input and submit
+    answer_a, answer_b = main(input)
+    print(f"Your input answers: \nA: {answer_a}\nB: {answer_b}")
+
+    submit_answer(answer_a, "a")
+    submit_answer(answer_b, "b")
